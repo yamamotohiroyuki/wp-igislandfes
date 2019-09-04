54a666e8407e453080b952a85dbe8bf5ba355b92
山本 博之 <bigupsanche@gmail.com>
2019-09-04 12:00:35 +0900

    meta修正

--- header.php
+++ header.php
@@ -5,11 +5,11 @@
   <meta name="viewport" content="width=device-width, initial-scale=1" />
   <link rel="profile" href="https://gmpg.org/xfn/11" />
   
-  <link rel="apple-touch-icon" sizes="180x180" href="<?php echo temp_add('img/meta') ?>/apple-touch-icon.png">
-  <link rel="icon" type="image/png" sizes="32x32" href="<?php echo temp_add('img/meta') ?>/favicon-32x32.png">
-  <link rel="icon" type="image/png" sizes="16x16" href="<?php echo temp_add('img/meta') ?>/favicon-16x16.png">
-  <link rel="manifest" href="<?php echo temp_add('img/meta') ?>/site.webmanifest">
-  <link rel="mask-icon" href="<?php echo temp_add('img/meta') ?>/safari-pinned-tab.svg" color="#2ca6e0">
+  <link rel="apple-touch-icon" sizes="180x180" href="<?php echo temp_add('/meta') ?>/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="<?php echo temp_add('/meta') ?>/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="<?php echo temp_add('/meta') ?>/favicon-16x16.png">
+  <link rel="manifest" href="<?php echo temp_add('/meta') ?>/site.webmanifest">
+  <link rel="mask-icon" href="<?php echo temp_add('/meta') ?>/safari-pinned-tab.svg" color="#2ca6e0">
   <meta name="msapplication-TileColor" content="#2ca6e0">
   <meta name="theme-color" content="#ffffff">
   <?php wp_head(); ?>
