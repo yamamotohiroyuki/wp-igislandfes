8cf08cdfbca5a89c7ee40417e7231740f13fe3af
山本 博之 <bigupsanche@gmail.com>
2019-09-12 21:50:49 +0900

    文字修正

--- inc/underconstruction.php
+++ inc/underconstruction.php
@@ -1,3 +1,10 @@
 <div class="underconstruction">
-  <p><img src="<?php echo temp_add('img/underconstruction') ?>/underconstruction.png" alt="underconstruction" class="shake-little"></p>
+  <?php
+    if(is_page('access')):
+      $file_name = 'underconstruction-yet';
+    else:
+      $file_name = 'underconstruction';
+    endif;
+  ?>
+  <p><img src="<?php echo temp_add('img/underconstruction/').$file_name ?>" alt="underconstruction" class="shake-little"></p>
 </div>
