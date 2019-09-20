bb479181fff0f460e49826a0e039e387da59197c
山本 博之 <bigupsanche@gmail.com>
2019-09-21 00:19:23 +0900

    contact

--- inc/thema-footer.php
+++ inc/thema-footer.php
@@ -3,7 +3,7 @@
     <p class="global-footer__brand"><a href="<?php echo home_url() ?>"><img src="<?php echo temp_add('img/global') ?>/brand-igislandfes-2019-full-white.svg" alt="IG ISLAND FES 2019 in Ishigaki"></a></p>
     <!-- nav class="global-footer__sns">
       <ul>
-        <li><a href="https://www.facebook.com/<?php the_field('global_facebook_url','option'); ?>/" class="global-footer__sns--facebook" target="_blank"></a></li>
+        <li><a href="https://www.facebook.com/<?php the_field('link_facebook','option'); ?>/" class="global-footer__sns--facebook" target="_blank"></a></li>
       </ul>
     </nav><!-- / .global-footer__sns -->
   </div><!-- / .global-footer__header -->
